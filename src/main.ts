2ec485e195126d10235b94cd32129f8186039a4b
App-Ju <appjuice@mail.ru>
2022-06-06 14:58:48 +0700

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
+import egalWidgets from "@egalteam/widget-library";
+import "@egalteam/widget-library/dist/style.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).use(egalWidgets).mount("#app");
