40d41fb778c4028656b54cd355f71989f9ec7118
App-Ju <appjuice@mail.ru>
2022-06-07 17:36:44 +0700

    added ShoppingCartView.vue

---
+++ src/views/ShoppingCartView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>sdafs</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ShoppingCart",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped></style>
