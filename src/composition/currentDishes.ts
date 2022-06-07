bc7c7d5be1562d03cff07627af7f30ff76a5f72d
App-Ju <appjuice@mail.ru>
2022-06-07 17:33:48 +0700

    added composition component currentDishes.ts

---
+++ src/composition/currentDishes.ts
@@ -0,0 +1,15 @@
+import { useRouter } from "vue-router";
+import { useRestaurantsStore } from "@/store/restaurants";
+import { IRestaurant } from "@/typescript/interfaces";
+
+export function useCurrentDishes() {
+  const router = useRouter();
+  const restaurantsStore = useRestaurantsStore();
+
+  const showDishes = (restaurant: IRestaurant) => {
+    restaurantsStore.addCurrentDishes(restaurant.id);
+    router.push(`/dishes`);
+  };
+
+  return { showDishes };
+}
