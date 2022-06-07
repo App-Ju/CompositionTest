9b8126f89bb5c420a96302ccb4f02d141723b267
App-Ju <appjuice@mail.ru>
2022-06-07 17:34:57 +0700

    upd: Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import RestaurantView from "../views/RestaurantView.vue";
 import DishesView from "../views/DishesView.vue";
+import ShoppingCartView from "../views/ShoppingCartView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "restaurant",
+    component: RestaurantView,
   },
   {
-    path: "/dishes/:id",
+    path: "/dishes",
     name: "dishes",
     component: DishesView,
+  },
+  {
+    path: "/shopping-cart",
+    name: "shopping-cart",
+    component: ShoppingCartView,
   },
 ];
 
