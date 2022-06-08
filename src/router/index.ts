1ccfb30956383e152184ba0ac894317a92151c76
App-Ju <appjuice@mail.ru>
2022-06-08 13:14:53 +0700

    added hook "beforeEnter" for correct display of pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { useRestaurantsStore } from "@/store/restaurants";
 import RestaurantView from "../pages/RestaurantPage.vue";
 import OrderPage from "../pages/OrderPage.vue";
 
@@ -12,6 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     path: "/order/:id",
     name: "order",
     component: OrderPage,
+    beforeEnter: (to, from) => {
+      const restaurantsStore = useRestaurantsStore();
+      if (!restaurantsStore.currentRestaurantDishes.length) {
+        return { name: "restaurant" };
+      }
+    },
   },
 ];
 
