9881441357aba362a1429e69c314ecab1ab383ad
AppJu <appjuice@mail.ru>
2022-06-08 09:56:05 +0700

    more reusable DishCard.vue

--- src/components/DishCard.vue
+++ src/components/DishCard.vue
@@ -1,142 +1,37 @@
 <template>
-  <div class="dish" @mouseenter="toggleVisible" @mouseleave="toggleVisible">
-    <img src="../assets/logo.png" alt="#" class="dish__img" />
-    <div class="dish__body">
-      <div class="dish__name">{{ dish.name }}</div>
-      <div v-show="isVisible" class="dish__description">
-        {{ dish.description }}
+  <div :style="dishStyle" @mouseenter="onVisible" @mouseleave="offVisible">
+    <slot name="img" />
+    <div :style="bodyStyle">
+      <slot name="name" />
+      <div v-show="isVisible" style="z-index: 1000">
+        <slot name="description" />
       </div>
-      <div class="dish__footer">
-        <div class="dish__price">Цена: {{ dish.price }}₽</div>
-        <EButton
-          v-if="!dish.quantityInCart"
-          id="cart-button"
-          :data="{
-            size: 'xs',
-          }"
-          @click="addQuantity(dish)"
-          >В корзину</EButton
-        >
-        <div v-else class="dish__footer-btn">
-          <BootstrapIcon icon="plus-square" @click="addQuantity(dish)" />
-          <div>{{ dish.quantityInCart }}</div>
-          <BootstrapIcon icon="dash-square" @click="reduceQuantity(dish)" />
-        </div>
-      </div>
+      <slot name="price" />
+      <slot name="buttons" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent } from "vue";
 import { useShowContent } from "@/composition/showContent";
-import { useChangeQuantity } from "@/composition/changeQuantity";
-import { IDish } from "@/typescript/interfaces";
-import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
 
 export default defineComponent({
   name: "DishCard",
-  components: { BootstrapIcon },
   setup() {
-    const { isVisible, toggleVisible } = useShowContent();
-    const { addQuantity, reduceQuantity } = useChangeQuantity();
+    const { isVisible, onVisible, offVisible } = useShowContent();
 
-    return { isVisible, toggleVisible, addQuantity, reduceQuantity };
+    return { isVisible, onVisible, offVisible };
   },
   props: {
-    dish: {
-      type: Object as PropType<IDish>,
+    bodyStyle: {
+      type: Object,
       require: true,
     },
-  },
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {
-    add(dish: IDish) {
-      console.log(dish);
+    dishStyle: {
+      type: Object,
+      require: true,
     },
   },
 });
 </script>
-
-<style lang="scss" scoped>
-@import "@/assets/style/variables.scss";
-
-.dish {
-  position: relative;
-  min-width: 160px;
-  width: calc(100% / 4 - (#{$margin} + #{$padding}) * 2);
-  height: 220px;
-  margin: $margin;
-  padding: $padding;
-  background: $background-color;
-  border-radius: 8px;
-
-  &__img {
-    position: absolute;
-    left: 0;
-    top: 10px;
-    width: 100%;
-    height: 60%;
-    object-fit: cover;
-    z-index: 0;
-  }
-
-  &__body {
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: start;
-    width: 100%;
-    height: 100%;
-    z-index: 1000;
-  }
-
-  &__name {
-    width: 100%;
-    padding-top: 8px;
-    text-align: start;
-    background: $background-color;
-    z-index: 1000;
-  }
-
-  &__description {
-    width: 100%;
-    padding-top: 8px;
-    font-size: 12px;
-    text-align: start;
-    background: $background-color;
-    z-index: 1000;
-  }
-
-  &__footer {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 15%;
-    width: 100%;
-  }
-
-  &__price {
-    font-size: 14px;
-  }
-
-  &__footer-btn {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 75px;
-    height: 26px;
-  }
-}
-
-#cart-button {
-  background-color: #57b750;
-
-  &:active {
-    background-color: #7ada73;
-  }
-}
-</style>
