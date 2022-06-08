1e4940f60fd2ecff29b5d83bfc45cea524252eb7
App-Ju <appjuice@mail.ru>
2022-06-08 13:19:20 +0700

    upd: css styles added totalCount for Shopping cart icon

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,9 @@
 <template>
   <nav class="nav">
     <router-link to="/">Все Рестораны</router-link>
-    <router-link to="/order">
+    <router-link to="/order/restaurantId" class="nav__cart">
       <BootstrapIcon id="nav-cart" icon="cart4" />
+      <div v-if="totalCount" class="nav__cart-count">{{ totalCount }}</div>
     </router-link>
   </nav>
 </template>
@@ -10,26 +11,50 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
+import { useSelectedDishes } from "@/composition/selectedDishes";
+import { useCurrentRestaurant } from "@/composition/currentRestaurant";
 
 export default defineComponent({
   name: "HeaderComponent",
   components: { BootstrapIcon },
+  setup() {
+    const { totalCount } = useSelectedDishes();
+    const { restaurantId } = useCurrentRestaurant();
+
+    return { totalCount, restaurantId };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 .nav {
-  padding: 25px 15px;
+  padding: 15px 15px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
+  background-color: #eaebee;
+
+  &__cart {
+    position: relative;
+  }
+
+  &__cart-count {
+    position: absolute;
+    right: -2px;
+    bottom: 0;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    font-size: 12px;
+    background-color: #57b750;
+  }
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
-    &.router-link-exact-active {
+    &:hover {
       color: #42b983;
     }
   }
