648d711fe41cd4a53f4bc47adeb84a9f85100775
App-Ju <appjuice@mail.ru>
2022-06-08 13:17:40 +0700

    renamed from currentDishes added variable restaurantId

--- src/composition/currentRestaurant.ts
+++ src/composition/currentRestaurant.ts
@@ -1,15 +1,19 @@
 import { useRouter } from "vue-router";
 import { useRestaurantsStore } from "@/store/restaurants";
 import { IRestaurant } from "@/typescript/interfaces";
+import { ref } from "vue";
 
-export function useCurrentDishes() {
+export function useCurrentRestaurant() {
   const router = useRouter();
   const restaurantsStore = useRestaurantsStore();
 
+  const restaurantId = ref();
+
   const showDishes = (restaurant: IRestaurant) => {
+    restaurantId.value = restaurant.id;
     restaurantsStore.addCurrentDishes(restaurant.id);
     router.push(`/order/${restaurant.id}`);
   };
 
-  return { showDishes };
+  return { restaurantId, showDishes };
 }
